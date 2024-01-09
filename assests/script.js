6bfae45eb6996fbbf1ab8d8041a0eeba0e0d6050
Inez Carrillo <inezcarrillo@Inezs-MacBook-Pro.local>
2024-01-09 13:47:27 -0600

    added to both javascrips

--- assests/script.js
+++ assests/script.js
@@ -1,6 +1,13 @@
 var searchBtn = document.querySelector("#searchBtn");
+var newText = document.querySelectorAll('#searchBar');
 
 searchBtn.addEventListener("click", nextPage);
+
+function nextPage() {
+    document.getElementById("click").innerHTML;
+    var newText = searchBar.value;
+    console.log(newText);
+}
 
 function nextPage() {
   window.location.assign('search-results.html');
--- assests/search-results.js
+++ assests/search-results.js
@@ -1,8 +1,25 @@
 var searchBtn = document.querySelector('#searchBtn');
+var dallasWeatherUrl = "http://api.openweathermap.org/geo/1.0/direct?q=Dallas,TX,+1&limit=5&appid=211c2be22c6d4ad1fb1d00f6bfd8f732"
+var newText = document.querySelectorAll('#searchBar');
 
+
+searchBtn = addEventListener("click", respondClick)
+
+function respondClick() {
+    document.getElementById("click").innerHTML;
+    var newText = searchBar.value;
+    console.log(newText);
+}
 
 async function cityWeather() {
-    const response = await fetch("http://example.com/movies.json");
+    const response = await fetch("http://api.openweathermap.org/geo/1.0/direct?q=Dallas,TX,+1&limit=5&appid=211c2be22c6d4ad1fb1d00f6bfd8f732");
     const weather = await response.json();
     console.log(weather);
   }
+  fetch("http://api.openweathermap.org/geo/1.0/direct?q=Dallas,TX,+1&limit=5&appid=211c2be22c6d4ad1fb1d00f6bfd8f732")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
