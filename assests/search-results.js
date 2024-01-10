da2387cf157e9ac0674183ae07a0d1766f8304cb
Inez Carrillo <inezcarrillo@Inezs-MacBook-Pro.local>
2024-01-09 21:21:20 -0600

    creating api key

--- assests/search-results.js
+++ assests/search-results.js
@@ -1,15 +1,20 @@
-var searchBtn = document.querySelector('#searchBtn');
-var dallasWeatherUrl = "http://api.openweathermap.org/geo/1.0/direct?q=Dallas,TX,+1&limit=5&appid=211c2be22c6d4ad1fb1d00f6bfd8f732"
-var newText = document.querySelectorAll('#searchBar');
+var searchBtn = document.getElementById('#searchBtn');
+var newText = document.getElementById('#searchBar');
+var today = moment().format('L');
+var searchHistoryList = [];
+
 
 
 searchBtn = addEventListener("click", respondClick)
 
 function respondClick() {
     document.getElementById("click").innerHTML;
-    var newText = searchBar.value;
-    console.log(newText);
+    var newText = newText.value;
+    console.log("success");
 }
+
+var apiKey = '211c2be22c6d4ad1fb1d00f6bfd8f732';
+
 
 async function cityWeather() {
     const response = await fetch("http://api.openweathermap.org/geo/1.0/direct?q=Dallas,TX,+1&limit=5&appid=211c2be22c6d4ad1fb1d00f6bfd8f732");
