a26fc62ca6562eded1c2031cbfbd23d9d6b6c2be
Inez Carrillo <inezcarrillo@Inezs-MacBook-Pro.local>
2024-01-10 14:07:11 -0600

    corrected some erros

--- assests/search-results.js
+++ assests/search-results.js
@@ -1,23 +1,35 @@
-var searchBtn = document.getElementById('#searchBtn');
-var newText = document.getElementById('#searchBar');
+var searchBtn = document.getElementById('searchBtn');
+var newText = document.getElementById('searchBar');
 var searchHistoryList = [];
-
-
-
-searchBtn = addEventListener("click", respondClick)
-
-function respondClick() {
-    //document.getElementById("click").innerHTML;
-    var newText = newText.value;
-    console.log("success");
-}
-
 var apiKey = '211c2be22c6d4ad1fb1d00f6bfd8f732';
 
-function cityWeather(cityName) {
-    var weatherURL = "api.openweathermap.org/data/2.5/forecast?" + cityName + "&appid=" + apiKey;
-    axios.get(weatherURL)
-    .then(function (response) {
 
-    })
+
+//searchBtn.addEventListener("click", respondClick);
+
+function respondClick() {
+    var cityName = newText.value;
+    console.log("City Name: ", cityName);
+    cityWeather(cityName);
 }
+
+// fetch response for open weather 
+function cityWeather(cityName) {
+    var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
+
+    fetch(weatherURL)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error');
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Process the retrieved weather data
+            console.log('Weather Data:', data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
